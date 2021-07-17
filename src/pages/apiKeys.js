c7ec8575b4c809f313894e0ad28b51d47d42dde5
johannpereze <sebas.jsp@gmail.com>
2021-07-17 13:31:28 -0500

    Agregados formulario de contraseñas

--- src/pages/apiKeys.js
+++ src/pages/apiKeys.js
@@ -1,6 +1,27 @@
 const passwords = ()=>{
     const view = `
-    <h1>Password</h1>
+    <h2>Claves de acceso:</h2>
+
+<section class="apikeys-container">
+
+    <form action="">
+        <label for="dentalink-apikey">
+            <span>Ingresa la clave de dentalink:</span>
+            <input type="text" name="" id="dentalink-apikey">
+        </label>
+        <label for="B2Chat-apikey">
+            <span>Ingresa el usuario de B2Chat:</span>
+            <input type="text" name="" id="B2Chat-apikey">
+        </label>
+        <label for="B2Chat-apipass">
+            <span>Ingresa la clave de B2Chat:</span>
+            <input type="text" name="" id="B2Chat-apipass">
+        </label>
+        
+    </form>
+    
+</section>
+
     <a href="/#/line"><button class="button">Siguiente</button></a>
     ` 
     return view
